4e56bdb69f260ad84ff19e4d30ac1073bb4fac8e
Abda Shaffan Diva <abdashaffan@gmail.com>
2018-10-31 21:40:03 +0700

    Edit existing libraries

---
+++ Header Files/variables.h
@@ -0,0 +1,21 @@
+/* DAFTAR LIBRARY ADT YANG DIGUNAKAN */
+
+#include "array.h"
+#include "jam.h"
+#include "listdp.h"
+#include "listlinier.h"
+#include "listsirkuler.h"
+#include "matriks.h"
+#include "mesinkata.h"
+#include "point.h"
+#include "queue.h"
+#include "stackt.h"
+
+/* Ada cara buat masukin semua ADT ke satu header gitu gak ya? */
+
+
+extern JAM PlayTime;
+extern int Money;
+extern Table TableList[20];
+extern int PlayerLife;
+extern long PlayerMoney;
---
+++ Source Files/variables.c
@@ -0,0 +1,10 @@
+//Nama Kelompok/Kelas : Cisitu plus Nobel / K03
+
+#include "variables.h"
+
+JAM PlayTime;
+int Money;
+Table TableList[20];
+int PlayerLife;
+long PlayerMoney;
+//Food TheFood;
