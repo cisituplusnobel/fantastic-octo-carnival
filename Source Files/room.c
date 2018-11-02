2a2b87f17c3bf6985bd05099891cfd40f9bc5067
agentnine <juniardiakbar@gmail.com>
2018-11-02 11:19:59 +0700

    Update room.c

--- Source Files/room.c
+++ Source Files/room.c
@@ -4,6 +4,7 @@
 #include "point.h"
 #include "jam.h"
 #include "queue.h"
+#include "datatype.h"
 
 #define RMin 1
 #define RMax 100
@@ -61,7 +62,7 @@ void MakeEmptyTable(ROOM *R, int room, int table)
   }
 }
 
-void MakeFullTable(ROOM *R, int room, int table)
+void MakeFullTable4(ROOM *R, int room, int table)
 /**/
 
 {
@@ -71,36 +72,67 @@ void MakeFullTable(ROOM *R, int room, int table)
     Elmt(Elm(*R,1),2,3) = 'C';
     Elmt(Elm(*R,1),3,2) = 'C';
     Elmt(Elm(*R,1),2,2) = '1';
-  } else if (room==1 && table==2){
-    Elmt(Elm(*R,1),2,6) = 'C';
-    Elmt(Elm(*R,1),2,8) = 'C';
-    Elmt(Elm(*R,1),2,7) = '2';
   } else if (room==1 && table==3){
     Elmt(Elm(*R,1),6,2) = 'C';
     Elmt(Elm(*R,1),7,1) = 'C';
     Elmt(Elm(*R,1),7,3) = 'C';
     Elmt(Elm(*R,1),8,2) = 'C';
-    Elmt(Elm(*R,1),7,2) = '3';
-  } else if (room==1 && table==4){
-    Elmt(Elm(*R,1),7,6) = 'C';
-    Elmt(Elm(*R,1),7,8) = 'C';
-    Elmt(Elm(*R,1),7,7) = '4';      
+    Elmt(Elm(*R,1),7,2) = '3';      
   } else if ((room==2 || room==3) && table==1){
     Elmt(Elm(*R,room),1,3) = 'C';
     Elmt(Elm(*R,room),2,2) = 'C';
     Elmt(Elm(*R,room),2,4) = 'C';
     Elmt(Elm(*R,room),3,3) = 'C';
-    Elmt(Elm(*R,room),2,3) = '1'; 
-  } else if ((room==2 || room==3) && table==2){
-    Elmt(Elm(*R,room),2,6) = 'C';
-    Elmt(Elm(*R,room),2,8) = 'C';
-    Elmt(Elm(*R,room),2,7) = '2';
+    Elmt(Elm(*R,room),2,3) = '1';
   } else if ((room==2 || room==3) && table==3){
     Elmt(Elm(*R,room),6,2) = 'C';
     Elmt(Elm(*R,room),7,1) = 'C';
     Elmt(Elm(*R,room),7,3) = 'C';
     Elmt(Elm(*R,room),8,2) = 'C';
     Elmt(Elm(*R,room),7,2) = '3';
+  }
+}
+
+void MakeFullTable2(ROOM *R, int room, int table)
+/**/
+
+{
+  if (room==1 && table==1){
+    Elmt(Elm(*R,1),1,2) = 'X';
+    Elmt(Elm(*R,1),2,1) = 'C';
+    Elmt(Elm(*R,1),2,3) = 'C';
+    Elmt(Elm(*R,1),3,2) = 'X';
+    Elmt(Elm(*R,1),2,2) = '1';
+  } else if (room==1 && table==3){
+    Elmt(Elm(*R,1),6,2) = 'X';
+    Elmt(Elm(*R,1),7,1) = 'C';
+    Elmt(Elm(*R,1),7,3) = 'C';
+    Elmt(Elm(*R,1),8,2) = 'X';
+    Elmt(Elm(*R,1),7,2) = '3';      
+  } else if ((room==2 || room==3) && table==1){
+    Elmt(Elm(*R,room),1,3) = 'X';
+    Elmt(Elm(*R,room),2,2) = 'C';
+    Elmt(Elm(*R,room),2,4) = 'C';
+    Elmt(Elm(*R,room),3,3) = 'X';
+    Elmt(Elm(*R,room),2,3) = '1';
+  } else if ((room==2 || room==3) && table==3){
+    Elmt(Elm(*R,room),6,2) = 'X';
+    Elmt(Elm(*R,room),7,1) = 'C';
+    Elmt(Elm(*R,room),7,3) = 'C';
+    Elmt(Elm(*R,room),8,2) = 'X';
+    Elmt(Elm(*R,room),7,2) = '3';
+  } else if (room==1 && table==2){
+    Elmt(Elm(*R,1),2,6) = 'C';
+    Elmt(Elm(*R,1),2,8) = 'C';
+    Elmt(Elm(*R,1),2,7) = '2';
+  } else if (room==1 && table==4){
+    Elmt(Elm(*R,1),7,6) = 'C';
+    Elmt(Elm(*R,1),7,8) = 'C';
+    Elmt(Elm(*R,1),7,7) = '4';      
+  } else if ((room==2 || room==3) && table==2){
+    Elmt(Elm(*R,room),2,6) = 'C';
+    Elmt(Elm(*R,room),2,8) = 'C';
+    Elmt(Elm(*R,room),2,7) = '2';
   } else if ((room==2 || room==3) && table==4){
     Elmt(Elm(*R,room),7,6) = 'C';
     Elmt(Elm(*R,room),7,8) = 'C';
@@ -133,7 +165,7 @@ boolean IsKosong(ROOM R, int room, int table)
 /**/
 {
   if (room==1 && table==1){
-    if (Elmt(Elm(R,1),1,2) == 'C'){
+    if (Elmt(Elm(R,1),2,1) == 'C'){
       return false;
     }else{
       return true;
@@ -145,7 +177,7 @@ boolean IsKosong(ROOM R, int room, int table)
       return true;
     }
   } else if (room==1 && table==3){
-    if (Elmt(Elm(R,1),6,2) == 'C'){
+    if (Elmt(Elm(R,1),7,1) == 'C'){
       return false;
     }else{
       return true;
@@ -157,7 +189,7 @@ boolean IsKosong(ROOM R, int room, int table)
       return true;
     }
   } else if ((room==2 || room==3) && table==1){
-    if (Elmt(Elm(R,room),1,3) == 'C'){
+    if (Elmt(Elm(R,room),2,2) == 'C'){
       return false;
     }else{
       return true;
@@ -169,13 +201,13 @@ boolean IsKosong(ROOM R, int room, int table)
       return true;
     }
   } else if ((room==2 || room==3) && table==3){
-    if (Elmt(Elm(R,room),6,2) == 'C'){
+    if (Elmt(Elm(R,room),7,1) == 'C'){
       return false;
     } else{
       return true;
     }
   } else if ((room==2 || room==3) && table==4){
-    if (Elmt(Elm(R,room),7,6) = 'C'){
+    if (Elmt(Elm(R,room),7,6) == 'C'){
       return false;
     }else{
       return true;
@@ -218,10 +250,24 @@ void IsKosong2(ROOM R, int *room, int *table)
   //ALGORITMA
   i=1;
   kosong = false;
+  *room = -999;
+  *table = -999;
   while (i<=3 && !kosong){
     j=2;
-    *room = -999;
-    *table = -999;
+    while (j<=4 && !kosong){
+      if (IsKosong(R,i,j)){
+        kosong = true;
+        *room = i;
+        *table = j;
+      }else{
+        j=j+2;
+      }
+    }
+    i++;
+  }
+  i=1;
+  while (i<=3 && !kosong){
+    j=1;
     while (j<=4 && !kosong){
       if (IsKosong(R,i,j)){
         kosong = true;
@@ -235,7 +281,7 @@ void IsKosong2(ROOM R, int *room, int *table)
   }
 }
 
-void Inisalisasi(ROOM *R, POINT *P, JAM *J, Queue *Q)
+void Inisalisasi(ROOM *R, POINT *P, JAM *J, Queue *Q, Customer cust)
 /* Melakukan inisalisasi pada bentuk setiap room
 yaitu ruangan */
 {
@@ -260,8 +306,10 @@ yaitu ruangan */
   MakeKitchen(R);
   *P = MakePOINT(4,4);
   *J = MakeJAM(0,0,0);
-  CreateEmpty(Q,5);
-  Add(Q, 4);
+  CreateEmptyQueue(Q,10);
+  cust.NbPeople = 4;
+  cust.WaitTime = 30;
+  AddQueue(Q, cust);
 }
 
 int main(){
@@ -271,17 +319,18 @@ int main(){
   JAM J;
   Queue Q;
   char C1,C2;
-  int i,X,room,ruang,meja;
+  int i,ii,j,k,room,ruang,meja;
+  boolean boolq;
+  Customer cust,X;
 
   //ALGORITMA
-  Inisalisasi(&R, &P, &J, &Q);
+  Inisalisasi(&R, &P, &J, &Q,cust);
   Elmt(Elm(R,1),Absis(P),Ordinat(P)) = 'P';
   room = 1;
   TulisMATRIKS(Elm(R,room));
   printf("\n");
   printf("COMMAND = ");
   scanf("%c%c", &C2, &C1);
-  
   while(true){
     if (C2=='D'){
       if (Absis(P) == 8 && Ordinat(P) == 5 && (room == 1 || room == 2)){
@@ -352,29 +401,62 @@ int main(){
       }
       J = NextDetik(J);
     }else if(C2=='P'){
-      if (!IsEmpty(Q)){
-        i=Head(Q);
-        while(i!=Tail(Q)){
-          if (InfoHead(Q) == 4){
-            (IsKosong4(R,&ruang,&meja));
-            if (ruang != -999){
-              MakeFullTable(&R,ruang,meja);
-              Del(&Q, &X);
-            }
-            else{
-              
-            }
-          }else{
+      if (!IsEmptyQueue(Q)){
+        if (InfoHead(Q).NbPeople == 4){
+          (IsKosong4(R,&ruang,&meja));
+          if (ruang != -999){
+            MakeFullTable4(&R,ruang,meja);
+            DelQueue(&Q, &X);
+          }
+          else{
             (IsKosong2(R,&ruang,&meja));
             if (ruang != -999){
-              MakeFullTable(&R,ruang,meja);
-              Del(&Q, &X);
-            }        
+              i=Head(Q);
+              boolq = false;
+              ii = Tail(Q)+1;
+              if (ii>MaxEl(Q)){
+                ii = ii-MaxEl(Q);
+              }
+              while ((i!=ii) && !boolq){
+                if (Cust(Q,i) == 2){
+                  MakeFullTable2(&R,ruang,meja);
+                  printf("\n\n--ADA--\n\n");
+                  boolq = true;
+                  j=i;
+                  while (j!=Tail(Q)){
+                    k=j+1;
+                    if (k>MaxEl(Q)){
+                      k = k-MaxEl(Q);
+                    }
+                    Q.T[j] = Q.T[k];
+                    j=k;
+                  }
+                  Tail(Q)--;
+                  if (Tail(Q)<1){
+                    Tail(Q) = MaxEl(Q);
+                  }
+                }
+                i++;
+                if (i>MaxEl(Q)){
+                  i = i-MaxEl(Q);
+                }
+              }
+            }
+            else{
+              printf("Semua Meja Penuh!\n");
+            }
           }
-          i++;
+        }else{
+          (IsKosong2(R,&ruang,&meja));
+          if (ruang != -999){
+            MakeFullTable2(&R,ruang,meja);
+            DelQueue(&Q, &X);
+          }else{
+            MakeFullTable4(&R,ruang,meja);
+          }    
         }
-        J = NextDetik(J);
       }
+      J = NextDetik(J);
     }else if(C2='T'){
       printf("RUANG 1 \n"); TulisMATRIKS(Elm(R,1)); printf("\n\n");
       printf("RUANG 2 \n"); TulisMATRIKS(Elm(R,2)); printf("\n\n");
@@ -387,33 +469,41 @@ int main(){
     TulisJAM(J);
     printf("ANTRIAN : \n");
 
-    if (!IsEmpty(Q)){
+    if (!IsEmptyQueue(Q)){
       if (Tail(Q) >= Head(Q)){
         for (i=Head(Q); i<= Tail(Q); i++){
-          printf("%d\n", Q.T[i]);
+          printf("%d\n", Cust(Q,i));
+          WT(Q,i)--;
         }
       } else{
         i=Head(Q);
         while (i!=Tail(Q)){
-          printf("%d\n", Q.T[i]);
+          printf("%d\n", Cust(Q,i));
           i++;
           if (i>MaxEl(Q)){
             i = i-MaxEl(Q);
+            WT(Q,i)--;
           }
         }
       }
     }
-
+    if (WT(Q,Head(Q))==0){
+      DelQueue(&Q,&X);
+      //printf("Deleted\n");
+    }
     printf("COMMAND = ");
     scanf("%c%c", &C2, &C1);
     if (JAMToDetik(J)%15 == 0){
       if (JAMToDetik(J)%2 == 0){
-        Add(&Q, 2);
+        cust.NbPeople = 4;
+        cust.WaitTime = 30;
       } else{
-        Add(&Q, 4);
+        cust.NbPeople = 2;
+        cust.WaitTime = 30;
       }
+      AddQueue(&Q, cust);
     }
+    
   }
-
   return 0;
 }
